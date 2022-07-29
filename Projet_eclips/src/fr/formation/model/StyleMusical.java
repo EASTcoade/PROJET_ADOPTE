9b6ad2c83825dc709661773f1b50d76877190f41
Danyl08 <smail.danyl@gmail.com>
2022-07-29 13:10:54 +0200

    initialisation de users

--- Projet_eclips/src/fr/formation/model/StyleMusical.java
+++ Projet_eclips/src/fr/formation/model/StyleMusical.java
@@ -2,6 +2,14 @@ package fr.formation.model;
 
 public class StyleMusical {
 	private String nom;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 	
 
 }
