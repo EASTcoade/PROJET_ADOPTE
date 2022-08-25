448cd5130e2657af3e67ca0535b59cc244b690d1
Danyl08 <smail.danyl@gmail.com>
2022-08-25 15:19:59 +0200

    service style musical

--- Projet_eclips/src/fr/formation/exception/ItemNotFoundException.java
+++ Projet_eclips/src/fr/formation/exception/ItemNotFoundException.java
@@ -1,5 +1,5 @@
 package fr.formation.exception;
 
-public class ItemlNotFoundException extends Exception {
+public class ItemNotFoundException extends Exception {
 	private static final long serialVersionUID = 1L;
 }
