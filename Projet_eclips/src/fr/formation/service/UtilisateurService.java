6961f91e9df923d683f0478851f0192b4aaf3de5
FouinLeopold <leopold.fouin@outlook.fr>
2022-08-25 15:37:51 +0200

    blablabla

---
+++ Projet_eclips/src/fr/formation/service/UtilisateurService.java
@@ -0,0 +1,68 @@
+package fr.formation.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.formation.exception.IdNegativeException;
+import fr.formation.exception.ItemNotFoundException;
+import fr.formation.model.Utilisateur;
+import fr.formation.repo.IUtilisateurRepository;
+import fr.formation.repo.sql.UtilisateurRepositorySql;
+
+
+
+public class UtilisateurService {
+	private IUtilisateurRepository repoUtilisateur = new UtilisateurRepositorySql();
+	
+	public List<Utilisateur> findAll() {
+		List<Utilisateur> utilisateurs = repoUtilisateur.findAll();
+		
+		if (utilisateurs == null) {
+			return new ArrayList<>();
+		}
+		
+		return utilisateurs;
+	}
+	
+	public Utilisateur findById(int id) throws IdNegativeException, ItemNotFoundException {
+		if (id <= 0) {
+			throw new IdNegativeException();
+		}
+		
+		Utilisateur utilisateur = repoUtilisateur.findById(id);
+		
+		if (utilisateur == null) {
+			throw new ItemNotFoundException();
+		}
+		
+		return utilisateur;
+	}
+	
+	public void save(Utilisateur utilisateur) {
+		if (utilisateur.getNom() == null || utilisateur.getNom().isBlank()) {
+			throw new NotValidException();
+		}
+		
+		if (utilisateur.getMdp() == null || utilisateur.getMdp().isBlank()) {
+			throw new NotValidException();
+		}
+		
+		if (utilisateur.getMail() == null || utilisateur.getMail().isBlank()) {
+			throw new NotValidException();
+		}
+		
+		
+		
+		repoUtilisateur.save(utilisateur);
+	}
+	
+	public void deleteById(int id) throws IdNegativeException {
+		if (id <= 0) {
+			throw new IdNegativeException();
+		}
+		
+		repoUtilisateur.deleteById(id);
+	}
+}
+
+
