e77aa5d49c473dbc30ed634ee8df97b1e1d4d592
FouinLeopold <leopold.fouin@outlook.fr>
2022-09-02 17:08:14 +0200

    rrdsf

--- PROJET_ADOPTE/src/main/java/fr/formation/model/Image.java
+++ PROJET_ADOPTE/src/main/java/fr/formation/model/Image.java
@@ -1,12 +1,15 @@
 package fr.formation.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.Table;
+@Entity
+@Table (name ="image")
 public class Image {
 	
 	@Enumerated(EnumType.ORDINAL)
--- PROJET_ADOPTE/src/main/java/fr/formation/model/Instrument.java
+++ PROJET_ADOPTE/src/main/java/fr/formation/model/Instrument.java
@@ -1,10 +1,13 @@
 package fr.formation.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.Table;
+@Entity
+@Table (name="instrument")
 public class Instrument {
 	
 	
