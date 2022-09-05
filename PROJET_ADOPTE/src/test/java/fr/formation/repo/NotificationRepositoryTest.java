9ffee2e13635c8e6a2d964e23122f3c23aa42a04
Etienne <etiennebacquet1518@gmail.com>
2022-09-05 17:25:50 +0200

    NotificationRepositoryTest

--- PROJET_ADOPTE/src/test/java/fr/formation/repo/NotificationRepositoryTest.java
+++ PROJET_ADOPTE/src/test/java/fr/formation/repo/NotificationRepositoryTest.java
@@ -1,12 +1,14 @@
 package fr.formation.repo;
 
 
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.UUID;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import fr.formation.model.Notification;
 import fr.formation.model.Notification;
 import fr.formation.repo.jpa.NotificationRepositoryJpa;
 
@@ -26,17 +28,42 @@ public class NotificationRepositoryTest {
 	
 	@Test
 	public void testFindById() {
-		Notification notif = this.notifRepo.findById(1);
+		Notification notif = this.notifRepo.findById(2);
 		Assertions.assertNotNull(notif);
-		Assertions.assertEquals(1, notif.getId());
+		Assertions.assertEquals(2, notif.getId());
 	}
 	
 	@Test
 	public void testDeleteById() {
-		this.notifRepo.deleteById(1);
-		Assertions.assertNull(this.notifRepo.findById(1));
+		this.notifRepo.deleteById(3);
+		Assertions.assertNull(this.notifRepo.findById(3));	
+	}
+	@Test
+	public void shouldAdd() {
+		Notification notification = new Notification();
+		String randomName = UUID.randomUUID().toString();
+		
+		notification.setMessage(randomName);
+		
+		this.notifRepo.save(notification);
+
+		Assertions.assertNotEquals(0, notification.getId());
+	}
+	
+	@Test
+	public void shouldUpdate() {
+		Notification notification = this.notifRepo.findById(2);
+		String ancien = notification.getMessage();
+		String randomName = UUID.randomUUID().toString();
+		
+		notification.setMessage(randomName);
 		
 		
+		this.notifRepo.save(notification);
+		
+		notification = this.notifRepo.findById(2);
+		
+		Assertions.assertNotEquals(ancien, notification.getMessage());
 	}
 }
 
