aa7ae844f443b7c43721b746cb85522cfcdb7e82
FouinLeopold <leopold.fouin@outlook.fr>
2022-09-05 17:05:16 +0200

    dsfsdf

--- PROJET_ADOPTE/src/test/java/fr/formation/repo/ImageRepositoryTest.java
+++ PROJET_ADOPTE/src/test/java/fr/formation/repo/ImageRepositoryTest.java
@@ -1,11 +1,19 @@
 package fr.formation.repo;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import fr.formation.model.FormatImage;
 import fr.formation.model.Image;
+import fr.formation.model.Niveau;
+import fr.formation.model.Utilisateur;
 import fr.formation.repo.jpa.ImageRepositoryJpa;
 import fr.formation.repo.jpa.SonRepositoryJpa;
 
@@ -32,4 +40,57 @@ public class ImageRepositoryTest {
 		this.repoImage.deleteById(1);
 		Assertions.assertNull(this.repoImage.findById(1));
 }
+	
+	@Test
+	public void shouldAdd() {
+		Image image = new Image();
+	
+		File f = new File("D:\\coding\\guitare.jpg");
+	
+		byte[] bytesFromFile = null;
+		try {
+			bytesFromFile = Files.readAllBytes(f.toPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		image.setId(1);
+		image.setContenu(bytesFromFile);
+		image.setTitre("premiere image");
+		image.setFormat(FormatImage.JPG);
+		
+		
+		this.repoImage.save(image);
+
+		Assertions.assertNotEquals(0, image.getId());
+	}
+	
+	@Test
+	public void shouldUpdate() {
+		Image image = this.repoImage.findById(2);
+		
+		byte[] ancien = image.getContenu();
+		
+		File f = new File("D:\\coding\\guitare.jpg");
+		
+		byte[] bytesFromFile = null;
+		try {
+			bytesFromFile = Files.readAllBytes(f.toPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		image.setContenu(bytesFromFile);
+		
+		
+		this.repoImage.save(image);
+		
+		image = this.repoImage.findById(2);
+		
+		Assertions.assertNotEquals(ancien, image.getContenu());
+	}
+	
+	
 }
