3ab52de4f594fc92de2caac1fedf254cd21d8bc5
Etienne <etiennebacquet1518@gmail.com>
2022-09-05 14:42:03 +0200

    ImageService

--- PROJET_ADOPTE/src/main/java/fr/formation/service/ImageService.java
+++ PROJET_ADOPTE/src/main/java/fr/formation/service/ImageService.java
@@ -1,5 +1,10 @@
 package fr.formation.service;
 
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,48 +13,68 @@ import fr.formation.exception.NotValidException;
 import fr.formation.exception.ImageNotFoundException;
 import fr.formation.model.Image;
 import fr.formation.repo.IImageRepository;
-import fr.formation.repo.ISonRepository;
 import fr.formation.repo.sql.ImageRepositorySql;
-import fr.formation.repo.sql.SonRepositorySql;
 
 public class ImageService {
-	public Image findById(int id) throws IdNegativeException, ImageNotFoundException {
-		IImageRepository repoImage = new ImageRepositorySql();
+//	public Image findById(int id) throws IdNegativeException, ImageNotFoundException {
+//		IImageRepository repoImage = new ImageRepositorySql();
+//		
+//		//verif de l'ID
+//		if(id<=0) {
+//			throw new IdNegativeException();
+//		}
+//		
+//		//V�rif du son
+//		Image lImage = repoImage.findById(id);
+//		if(lImage==null) {
+//			throw new ImageNotFoundException();
+//		}
+//		
+//		//on retourne le son trouv�
+//		return lImage;
+//	}
+//	public List<Image> findAll(){
+//		IImageRepository repoImage=new ImageRepositorySql();
+//		
+//		List<Image> listeImage =repoImage.findAll();
+//		if(listeImage==null) {
+//			return new ArrayList<>();
+//		}
+//		return listeImage;
+//	}
+//	public void save(Image images) throws NotValidException {
+//		IImageRepository repoImage=new ImageRepositorySql();
+//		if (images.getTitre() == null || images.getTitre().isBlank()) {
+//			throw new NotValidException();
+//		}
+//		
+//		if (images.getContenu() == null) {
+//			throw new NotValidException();
+//		}	
+//		
+//		repoImage.save(images);
+//	}
+	public String getFileContent(String image) {
+		Path path = Path.of(image);
 		
-		//verif de l'ID
-		if(id<=0) {
-			throw new IdNegativeException();
+		try {
+			String contenu = Files.readString(path);
+			return contenu;
 		}
 		
-		//V�rif du son
-		Image lImage = repoImage.findById(id);
-		if(lImage==null) {
-			throw new ImageNotFoundException();
+		catch (AccessDeniedException e) {
+			return "Accès refusé";
 		}
 		
-		//on retourne le son trouv�
-		return lImage;
+		catch (NoSuchFileException e) {
+			return "Fichier non trouvé";
+		}
+		
+		catch (IOException e) {
+			// Peut être intéressant à garder en "mode débug"
+//			e.printStackTrace(); // permet d'imprimer toute la trace d'erreur
+			return "Autre erreur de lecture / écriture";
+		}
 	}
-	public List<Image> findAll(){
-		IImageRepository repoImage=new ImageRepositorySql();
-		
-		List<Image> listeImage =repoImage.findAll();
-		if(listeImage==null) {
-			return new ArrayList<>();
-		}
-		return listeImage;
-	}
-	public void save(Image images) throws NotValidException {
-		IImageRepository repoImage=new ImageRepositorySql();
-		if (images.getTitre() == null || images.getTitre().isBlank()) {
-			throw new NotValidException();
-		}
-		
-		if (images.getContenu() == null) {
-			throw new NotValidException();
-		}	
-		
-		repoImage.save(images);
-	}
-
 }
+
