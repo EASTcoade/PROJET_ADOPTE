51ce5f57b1ca7663c96a5bec2845a85549de665b
RemGout <rem.goutagny@orange.fr>
2022-09-05 16:58:46 +0200

    StyleMusical à nouveau en classe

--- PROJET_ADOPTE/src/main/java/fr/formation/model/StyleMusical.java
+++ PROJET_ADOPTE/src/main/java/fr/formation/model/StyleMusical.java
@@ -9,10 +9,34 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Entity
+@Table(name="style_musical")
+public class StyleMusical {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="sty_id")
+	private int id;
+	
+	@Column(name="sty_nom")
+	protected String nom;
 
-public enum StyleMusical {
-	@Enumerated(EnumType.ORDINAL)
-	ROCK,POP,ELECTRO,CLASSIQUE,JAZZ,METAL,RAP,REGGAE;
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
 }
 	
 
